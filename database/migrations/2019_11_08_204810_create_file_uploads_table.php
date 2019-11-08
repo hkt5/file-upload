86b8b44673dc1e240baac921019fed03bb20a779
htkt5 <adrian.stolarski@gmail.com>
2019-11-08 21:53:07 +0100

    initial commit

---
+++ database/migrations/2019_11_08_204810_create_file_uploads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileUploadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_uploads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('file_name')->unique(true)->nullable(false);
+            $table->bigInteger('creator_id')->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_uploads');
+    }
+}
